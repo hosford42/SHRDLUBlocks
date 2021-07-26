31d9b533bad24f0c9471cbbed547e673582a6d61
Aaron Hosford <hosford42@gmail.com>
2021-07-25 23:57:13 -0500

    Added pyproject.toml, fleshed out setup.cfg

--- shrdlu_blocks/__init__.py
+++ shrdlu_blocks/__init__.py
@@ -1,1 +1,8 @@
+"""
+SHRDLU: A simple environment for evaluating and testing natural language understanding systems
+"""
+
 __version__ = '0.0'
+__author__ = 'Aaron Hosford'
+__author_email__ = 'hosford42@gmail.com'
+__url__ = 'https://github.com/hosford42/SHRDLUBlocks'
